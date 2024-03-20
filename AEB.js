0ba2f433a9b938c7ff72a7a852f21ad3e30a5979
divyasowmya <158157685+divyasowmya@users.noreply.github.com>
2024-03-20 16:11:13 +0530

    #  17607 AEB.js
    Implement logic to calculate the time to collision and determine if automatic braking is necessary

---
+++ AEB.js
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h1>Automated Emergency Breaking</h1>
+  <Script>[TASK-17607] Implement logic to calculate the time to collision and determine if automatic braking is necessary
+</script>
+</body>
+</html>
